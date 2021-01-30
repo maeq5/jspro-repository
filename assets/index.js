fe50002f13b365617bb640609b6c91b2ec6f819b
Miguel Angel Giulianetti Echeverry <miguel@examle.com>
2021-01-30 07:55:44 -0500

    AutoPlay Functionality Added

--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,8 @@
 import MediaPlayer from "./mediaPlayer.js";
+import AutoPlay from "./plugins/AutoPlay.js";
 
 const video = document.querySelector("video");
 const button = document.querySelector("button");
 
-const player = new MediaPlayer({el: video})
+const player = new MediaPlayer({el: video, plugins: [new AutoPlay()] });
 button.onclick = () => player.togglePlay();
